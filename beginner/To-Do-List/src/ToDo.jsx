62bdb83a9974164dd84183c8a8d4063437cf50b5
akashkhot12 <akashkhot03@gmail.com>
2024-05-14 13:21:00 +0530

    creating projects

---
+++ beginner/To-Do-List/src/ToDo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ToDo() {
+    const data = [""]
+  return (
+    <div>
+    <h1>To Do List App</h1>
+    <div>
+        <input type="text" placeholder='enter task in the field' />
+        <button>Add Task</button>
+    </div>
+    <div>
+        <ol>
+            <li>hello</li>
+        </ol>
+    </div>
+</div>
+  )
+}
