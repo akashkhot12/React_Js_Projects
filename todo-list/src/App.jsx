ad81f466b3be1055505158aebbca02db2eabeb9b
akashkhot12 <akashkhot03@gmail.com>
2024-05-15 12:48:18 +0530

    adding array

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -3,9 +3,25 @@ import "./App.css";
 import { FaCalendarTimes } from "react-icons/fa";
 import { FaCalendarPlus } from "react-icons/fa";
 
-
 export default function App() {
   const [isCompletedScreen, setIsCompletedScreen] = useState(false);
+
+  const [allTodos, setTodos] = useState([]);
+
+  const [newTitle, setNewTitle] = useState("");
+
+  const [newDescription, setNewDescription] = useState("");
+
+  const handleAddTodo = () => {
+    let newTodoItems = {
+      title: newTitle,
+      description: newDescription,
+    };
+
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newTodoItems);
+    setTodos(updatedTodoArr);
+  };
 
   return (
     <div>
@@ -15,14 +31,28 @@ export default function App() {
         <div className="todo-input">
           <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" placeholder="What is your task tittle ?" />
+            <input
+              type="text"
+              value={newTitle}
+              onClick={(e) => setNewTitle(e.target.value)}
+              placeholder="What is your task tittle ?"
+            />
           </div>
           <div className="todo-input-item">
             <label>Description</label>
-            <input type="text" placeholder="What is your task tittle ?" />
+            <input
+              type="text"
+              value={newDescription}
+              onClick={(e) => setNewDescription(e.target.value)}
+              placeholder="What is your task tittle ?"
+            />
           </div>
           <div className="todo-input-item">
-            <button type="button" className="primaryBtn">
+            <button
+              onClick={handleAddTodo}
+              type="button"
+              className="primaryBtn"
+            >
               Add
             </button>
           </div>
@@ -45,14 +75,18 @@ export default function App() {
           </button>
         </div>
         <div className="todo-list">
-          <div className="todo-list-item">
-            <h3>Task</h3>
-            <p>Description</p>
-            <div>
-              <FaCalendarTimes className="icon" />
-              <FaCalendarPlus className="check-icon" />
-            </div>
-          </div>
+          {allTodos.map((item, index) => {
+            return (
+              <div key={index} className="todo-list-item">
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <div>
+                  <FaCalendarTimes className="icon" />
+                  <FaCalendarPlus className="check-icon" />
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
